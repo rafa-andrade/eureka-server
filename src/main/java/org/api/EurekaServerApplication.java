f2bcd9be036edfbe8f5f9c96f7c8f749d9221e86
Rafael Andrade <rafaelolian@gmail.com>
2016-09-01 01:07:17 -0300

    eureka

---
+++ src/main/java/org/api/EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package org.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
